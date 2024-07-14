0c75d91c65c12c300af7ef7d479baa6e67b6efab
fjql <98269704+fjql@users.noreply.github.com>
2024-07-14 15:32:07 +0800

    Forgot to add title tags

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,6 +3,10 @@
   import WipTape from "$lib/components/wiptape.svelte"
 </script>
 
+<svelte:head>
+  <title>Error {$page.status}</title>
+</svelte:head>
+
 <main>
   <WipTape big={$page.status} msg={$page.error?.message} />
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,10 @@
   import WipTape from "$lib/components/wiptape.svelte";
 </script>
 
+<svelte:head>
+  <title>fjql</title>
+</svelte:head>
+
 <main>
   <WipTape big="Website Under Construction" msg="fjql"/>
 </main>
